bfd38449d5641983b7b41ee16c4b3b941ab2c522
Pstar123-blip <pstAr12321@proton.me>
2026-09-22 16:32:09 +0300

    deleted unnecessary db history/migration

--- api/drizzle/0001_nifty_mach_iv.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE "users" RENAME COLUMN "email" TO "username";--> statement-breakpoint
-ALTER TABLE "users" DROP CONSTRAINT "users_email_unique";--> statement-breakpoint
-ALTER TABLE "users" DROP COLUMN "permissions";--> statement-breakpoint
-ALTER TABLE "users" ADD CONSTRAINT "users_username_unique" UNIQUE("username");
